21ad616e2bdc150f4e7a71408fa9eb0e4a227af3
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-02-06 14:57:02 +0100

    up

--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -1,37 +1,37 @@
+import React, { useState, useEffect } from "react";
 import Cards from "../components/Cards";
 import Projects from "../assets/Json/projects.json";
-import { useEffect, useState } from "react";
 import NomPrenomAnimation from "../components/NomPrenomAnimation.jsx";
 
 const Realisations = () => {
-
-  const firstName = "Mes  projets ";
+  const firstName = "Mes projets";
+  const [projects, setProjects] = useState([]);
   const [animationTriggered, setAnimationTriggered] = useState(false);
 
   useEffect(() => {
     if (!animationTriggered) {
       setAnimationTriggered(true);
     }
+    // Charger les projets une seule fois lorsque le composant est monté
+    setProjects(Projects);
   }, [animationTriggered]);
 
   return (
     <>
-      
-        <section>
-          <NomPrenomAnimation string={firstName} speed={100} delay={1200} />
-        </section>
-        <section className="cardsBackground">
-          {Projects.map((project, index) => (
-            <Cards
-              key={index}
-              title={project.title}
-              subtitle={project.subtitle}
-              imageUrl={project.cover}
-              link={`/project/${project.id}`}
-            />
-          ))}
-        </section>
-     
+      <section>
+        <NomPrenomAnimation string={firstName} speed={100} delay={1200} />
+      </section>
+      <section className="cardsBackground">
+        {projects.map((project, index) => (
+          <Cards
+            key={index}
+            title={project.title}
+            subtitle={project.subtitle}
+            imageUrl={project.cover}
+            link={`/project/${project.id}`}
+          />
+        ))}
+      </section>
     </>
   );
 };
