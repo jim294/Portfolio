19cb6efb9a1fa69567536c4274ac6fefe3bb88ce
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-02-06 13:42:08 +0100

    up

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -5,7 +5,7 @@ import Collapse from "../components/Collapse";
 import Carousel from "../components/Carousel";
 import Projects from "../assets/Json/projects.json";
 import Preloader from "../components/Preloader.jsx";
-import CachePage from '../components/CachePage.jsx';
+
 
 const Project = () => {
   const [work, setWork] = useState(null);
@@ -25,7 +25,6 @@ const Project = () => {
     <Preloader />
   ) : (
     <>
-    <CachePage />
       <Carousel
         images={project.pictures}
         pagination={project.pictures.length}
