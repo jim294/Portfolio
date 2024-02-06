64de733731765b7ebb32e568e33d41b2ddeea1e0
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-02-06 14:27:35 +0100

    up

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -37,7 +37,7 @@ const Project = () => {
     return () => window.removeEventListener("beforeunload", handleReload);
   }, []);
 
-  return work == null ? (
+  return work == null || !project ? (
     <Preloader />
   ) : (
     <>
