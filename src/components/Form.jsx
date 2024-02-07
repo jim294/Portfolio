f5a41bdc35e70ccb0127e8e960950c4bb281d062
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-02-07 19:01:24 +0100

    upgrade FormData

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,11 +5,16 @@ emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
 
 const Form = () => {
   const [emailError, setEmailError] = useState("");
+  const [formData, setFormData] = useState({
+    nom: "",
+    prenom: "",
+    email: "",
+    message: "",
+  });
 
   const validateEmail = (email) => {
     // Expression régulière simple pour la validation d'un courriel
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
     return emailRegex.test(email);
   };
 
@@ -17,11 +22,7 @@ const Form = () => {
     e.preventDefault();
 
     // Collectez les données du formulaire
-    const formData = new FormData(e.target);
-    const nom = formData.get("nom");
-    const prenom = formData.get("prenom");
-    const email = formData.get("email");
-    const message = formData.get("message");
+    const { nom, prenom, email, message } = formData;
 
     // Valider l'email
     if (!validateEmail(email)) {
@@ -42,30 +43,71 @@ const Form = () => {
       })
       .then((response) => {
         alert("Email envoyé", response);
+
+        // Réinitialiser les valeurs du formulaire après l'envoi réussi
+        setFormData({
+          nom: "",
+          prenom: "",
+          email: "",
+          message: "",
+        });
       })
       .catch((error) => {
         console.error("Le mail ne s'est pas envoyé", error);
       });
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <form className="form" onSubmit={handleFormSubmit}>
       <div className="case">
         <label htmlFor="nom">Nom</label>
-        <input type="text" name="nom" id="nom" />
+        <input
+          type="text"
+          name="nom"
+          id="nom"
+          value={formData.nom}
+          onChange={handleChange}
+        />
       </div>
       <div className="case">
         <label htmlFor="prenom">Prénom</label>
-        <input type="text" name="prenom" id="prenom" />
+        <input
+          type="text"
+          name="prenom"
+          id="prenom"
+          value={formData.prenom}
+          onChange={handleChange}
+        />
       </div>
       <div className="case">
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
         {emailError && <p style={{ color: "red" }}>{emailError}</p>}
       </div>
       <div className="case">
         <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <textarea
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          value={formData.message}
+          onChange={handleChange}
+        ></textarea>
       </div>
       <input className="submit" type="submit" value="Envoyer" />
     </form>
