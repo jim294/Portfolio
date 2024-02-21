d0221196ee7816612ad66d6e30f516069db480c5
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-02-21 18:34:12 +0100

    minor modifications

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,40 +1,20 @@
-import { useEffect, useState } from "react";
-import { useMediaQuery } from "react-responsive";
-import NomPrenomAnimation from "../components/NomPrenomAnimation.jsx";
 import Form from "../components/Form";
+import ContactMe from '../assets/images/Contactez-moi.gif'
 
 const Contact = () => {
-  const firstName = "Me contacter";
-  const [animationTriggered, setAnimationTriggered] = useState(false);
-  const isSmallScreen = useMediaQuery({ maxWidth: 768 });
-
-  useEffect(() => {
-    if (!animationTriggered) {
-      setAnimationTriggered(true);
-    }
-  }, [animationTriggered]);
-
-  const getFontSize = () => {
-    return isSmallScreen ? "40px" : "80px";
-  };
-
   return (
     <>
       <main>
         <section>
           <div className="contact">
-            <NomPrenomAnimation
-              style={{ fontSize: getFontSize() }}
-              string={firstName}
-              speed={100}
-              delay={1200}
-            />
-            <h2>Une question ?</h2>
+            <img src={ContactMe} alt="contact GIF" />
+            <Form />
+            <div>
             <p>
               N’hésitez pas à m’écrire ! Je vous répondrai en moins de 24
               heures.
             </p>
-            <Form />
+            </div>
           </div>
         </section>
       </main>
--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -1,55 +1,38 @@
 import Cards from "../components/Cards";
 import Projects from "../assets/Json/projects.json";
-import { useEffect, useState } from "react";
-import NomPrenomAnimation from "../components/NomPrenomAnimation.jsx";
-import { useMediaQuery } from "react-responsive";
-import myGift  from '../assets/images/Travail.gif'
+import myGift from "../assets/images/Travail.gif";
+import myProject from "../assets/images/Mes-projets.gif";
 
 const Realisations = () => {
-  const firstName = "Mes  projets ";
-  const [animationTriggered, setAnimationTriggered] = useState(false);
-  const isSmallScreen = useMediaQuery({ maxWidth: 768 });
-
-  useEffect(() => {
-    if (!animationTriggered) {
-      setAnimationTriggered(true);
-    }
-  }, [animationTriggered]);
-
-  const getFontSize = () => {
-    return isSmallScreen ? "40px" : "80px";
-  };
-
   return (
     <>
       <section className="realisation">
-        <NomPrenomAnimation style={{ color: "black", fontSize: getFontSize() }} string={firstName} speed={100} delay={1200} />
-      
-      <img className="giftTravail" src={myGift} alt="Travail GIF" />
-      <h3>1/ OpenClassrooms</h3>
-      <section className="cardsBackground">
-        {Projects.Openclassrooms.map((project, index) => (
-          <Cards
-            key={index}
-            title={project.title}
-            subtitle={project.subtitle}
-            imageUrl={project.cover}
-            link={`/projectcours/${project.id}`}
-          />
-        ))}
-      </section>
-      <h3>2/ Conception personnelle</h3>
-      <section className="cardsBackground">
-        {Projects.Perso.map((project, index) => (
-          <Cards
-            key={index}
-            title={project.title}
-            subtitle={project.subtitle}
-            imageUrl={project.cover}
-            link={`/projectperso/${project.id}`}
-          />
-        ))}
-      </section>
+          <img src={myProject} alt="contact GIF" />
+          <img className="giftTravail" src={myGift} alt="Travail GIF" />
+        <h3>1/ OpenClassrooms</h3>
+        <section className="cardsBackground">
+          {Projects.Openclassrooms.map((project, index) => (
+            <Cards
+              key={index}
+              title={project.title}
+              subtitle={project.subtitle}
+              imageUrl={project.cover}
+              link={`/projectcours/${project.id}`}
+            />
+          ))}
+        </section>
+        <h3>2/ Conception personnelle</h3>
+        <section className="cardsBackground">
+          {Projects.Perso.map((project, index) => (
+            <Cards
+              key={index}
+              title={project.title}
+              subtitle={project.subtitle}
+              imageUrl={project.cover}
+              link={`/projectperso/${project.id}`}
+            />
+          ))}
+        </section>
       </section>
     </>
   );
