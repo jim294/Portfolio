4c6e2cfaaca9317e3f9dbda8cc64c6f0a9a90edb
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-02-22 20:48:43 +0100

    presentations

--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -1,49 +1,91 @@
 import Icones from "../assets/Json/icones.json";
-import myGiftPresentation from '../assets/images/présentation.gif'
+import myGiftPresentation from "../assets/images/présentation.gif";
+import besac from "../assets/images/Besancon_boucle_Doubs.webp";
+import diplome from "../assets/images/diplome.webp";
+import manWork from "../assets/images/manwork.webp";
+import openClassrooms from "../assets/images/openclassroomslogo.webp";
+import intelligenceImage from "../assets/images/intelligence.webp";
 
 const Presentation = () => {
   return (
     <>
       <main>
-        <img className="giftPresentation" src={myGiftPresentation} alt="Travail GIF" />
-          <h3>1-Présentation</h3>
-        <section className="cv_text">
-          <p>
-            Je m'appelle Jean-Marie DESCHAMPS, originaire de Besançon. Après
-            avoir obtenu avec succès une licence en commerce (Bac+3), j'ai
-            consacré plusieurs années à des responsabilités dans le secteur de
-            la grande distribution. Cependant, après avoir effectué un bilan de
-            compétences il y a quelques années, j'ai réalisé que le monde du
-            développement informatique représentait une véritable source
-            d'épanouissement pour moi.
-          </p>
-          <p>
-            Fort de cette prise de conscience, j'ai entrepris une reconversion
-            professionnelle, mettant en lumière ma passion de longue date pour
-            l'informatique, présente depuis mon plus jeune âge. Au cours de
-            cette année de reconversion, j'ai concentré mes efforts sur
-            l'apprentissage des langages informatiques de base, développant
-            ainsi les compétences nécessaires pour évoluer dans le domaine du
-            développement informatique.
-          </p>
-          <p>
-            Parallèlement, je suis actuellement en cours de formation en tant
-            qu'intégrateur web chez OpenClassroom, et je suis sur le point de
-            terminer mon parcours pour obtenir mon diplôme le 15 février
-            prochain. Passionné par le monde du développement web, je m'efforce
-            constamment de me perfectionner, avec un intérêt particulier pour
-            React, une bibliothèque JavaScript que je trouve particulièrement
-            puissante et captivante.
-          </p>
-          <p>
-            Mon parcours professionnel antérieur m'a doté de compétences en
-            gestion et en leadership, que je compte mettre à profit dans le
-            domaine informatique. Au-delà de l'apprentissage des technologies,
-            j'accorde une grande importance à la qualité de mon code. Je
-            considère que la clarté et la lisibilité du code sont essentielles,
-            et je m'efforce toujours de produire un travail soigné et bien
-            structuré.
-          </p>
+        <img
+          className="giftPresentation"
+          src={myGiftPresentation}
+          alt="Travail GIF"
+        />
+        <h3>1-Présentation</h3>
+        <section className="cv_text ">
+          <div className="photoPresentation">
+            <div>
+              <p>Je suis originaire de Besançon 🏙️.</p>
+            </div>
+            <div>
+              <img src={besac} alt="besancon" />
+            </div>
+          </div>
+          <div className="photoPresentationReverse">
+            <div>
+              <p>
+                Aprés avoir obtenu avec succès une licence en commerce (Bac+3) 👨‍🎓,
+                j'ai consacré plusieurs années à des responsabilités dans le
+                secteur de la grande distribution 🏪. Cependant, après avoir
+                effectué un bilan de compétences il y a quelques années, j'ai
+                réalisé que le monde du développement informatique représentait
+                une véritable source d'épanouissement pour moi 🤗.
+              </p>
+            </div>
+            <div>
+              <img src={diplome} alt="diplome" />
+            </div>
+          </div>
+          <div className="photoPresentation">
+            <div>
+              <p>
+                Fort de cette prise de conscience 💡, j'ai entrepris une
+                reconversion professionnelle, mettant en lumière ma passion de
+                longue date pour l'informatique, présente depuis mon plus jeune
+                âge. Au cours de cette année de reconversion, j'ai concentré mes
+                efforts sur l'apprentissage des langages informatiques de base,
+                développant ainsi les compétences nécessaires pour évoluer dans
+                le domaine du développement informatique.
+              </p>
+            </div>
+            <div>
+              <img src={manWork} alt="man at work" />
+            </div>
+          </div>
+          <div className="photoPresentationReverse">
+            <div>
+              <p>
+                Passionné par le monde du développement web, je m'efforce
+                constamment de me perfectionner, avec un intérêt particulier
+                pour React, une bibliothèque JavaScript que je trouve
+                particulièrement puissante et captivante. A ce titre, je viens
+                de valider ma formation d'integrateur Web chez OpenClassrooms.
+              </p>
+            </div>
+            <div>
+              <img src={openClassrooms} alt="openClassrooms" />
+            </div>
+          </div>
+          <div className="photoPresentation">
+            <div>
+              <p>
+                Mon parcours professionnel antérieur m'a doté de compétences en
+                gestion et en leadership, que je compte mettre à profit dans le
+                domaine informatique. Au-delà de l'apprentissage des
+                technologies, j'accorde une grande importance à la qualité de
+                mon code. Je considère que la clarté et la lisibilité du code
+                sont essentielles, et je m'efforce toujours de produire un
+                travail soigné et bien structuré.
+              </p>
+            </div>
+            <div>
+              <img src={intelligenceImage} alt="cerveau" />
+            </div>
+          </div>
           <p>
             Après l'obtention de mon diplôme, mon objectif principal est de
             continuer à me perfectionner, explorer de nouvelles technologies et
@@ -61,7 +103,7 @@ const Presentation = () => {
           </p>
         </section>
         <section className="iconesAll">
-        <h3>2-Skills</h3>
+          <h3>2-Skills</h3>
           <h4>- Languages & Databases -</h4>
           <div className="listIcones">
             {Icones.languages.map((item, index) => (
